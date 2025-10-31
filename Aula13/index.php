e7824ff2eaaa28282b4d5f88f161fe55437c3506
Marcelo Justen <Maaa.justen@gmail.com>
2025-10-30 21:11:40 -0300

    aula 13 - orientação ao objeto

---
+++ Aula13/index.php
@@ -0,0 +1,8 @@
+<?php
+    require_once "pessoa.php"/
+
+    $pessoa = new pessoa();
+    $pessoa ->setnome = ("Cleber");
+    $pessoa ->$sobrenome = "Nardelli";
+
+?>
---
+++ Aula13/pessoa.php
@@ -0,0 +1,23 @@
+<?php
+    class pessoa{
+        public function getNomeCompleto(){
+
+        }
+
+        public function getNome(){
+
+        }
+
+        public function getSobreNome(){
+
+        }
+
+        public function setNome(){
+
+        }
+
+        public function setSobreNome(){
+            
+        }
+    };
+?>
